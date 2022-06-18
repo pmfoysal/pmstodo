e21e42da4f903ba848211ededac6978570a59eb2
pmfoysal <pmfoysal@gmail.com>
2022-06-19 00:06:04 +0600

    Logo Component Added

---
+++ src/components/partials/logo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {LogoContainer} from './logo.styled';
+import {useLocation, useNavigate} from 'react-router-dom';
+
+export default function Logo() {
+   const navigate = useNavigate();
+   const {pathname} = useLocation();
+
+   function clickHandler() {
+      if (pathname !== '/') navigate('/');
+   }
+
+   return (
+      <LogoContainer onClick={clickHandler}>
+         Pm<span>Todo</span>
+      </LogoContainer>
+   );
+}
---
+++ src/components/partials/logo.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const LogoContainer = styled.h1`
+   font-size: 4.5rem;
+   line-height: 1;
+   user-select: none;
+   display: inline-block;
+   cursor: pointer;
+   transition: 0.1s ease;
+   color: ${({theme}) => theme.text.main.title};
+
+   span {
+      font-size: inherit;
+      line-height: inherit;
+      font-weight: inherit;
+      text-transform: capitalize;
+      color: ${({theme}) => theme.back.accent};
+      transition: inherit;
+   }
+
+   &:hover {
+      opacity: 0.85;
+   }
+
+   &:active {
+      transform: scale(0.95);
+   }
+`;
