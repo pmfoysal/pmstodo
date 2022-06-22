34c0fe5f60d72cc01945cb4f9650d655e0ce502a
pmfoysal <pmfoysal@gmail.com>
2022-06-22 11:56:11 +0600

    Confirm Component Added

---
+++ src/components/helpers/confirm.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Backdrop from './backdrop';
+import { Icon } from '@iconify/react';
+import Button from '@shared/button';
+import { ConfirmButtons, ConfirmContainer, ConfirmPara, ConfirmTitle } from './confirm.styled';
+
+export default function Confirm({ setActive, handler }) {
+   function cancelHandler() {
+      setActive(false);
+   }
+
+   function mainHandler() {
+      setActive(false);
+      if (handler) handler();
+   }
+
+   return (
+      <Backdrop>
+         <ConfirmContainer>
+            <ConfirmTitle>
+               confirmation!
+               <Icon onClick={cancelHandler} icon='ion:close-circle-outline' />
+            </ConfirmTitle>
+            <ConfirmPara>
+               Are you sure you want to delete it? Once deleted, this item can't be possible to bring back again!
+            </ConfirmPara>
+            <ConfirmButtons>
+               <Button name='cancel' neutral medium handler={cancelHandler} />
+               <Button name='delete' danger medium handler={mainHandler} />
+            </ConfirmButtons>
+         </ConfirmContainer>
+      </Backdrop>
+   );
+}
