5363fbbc1e095e8e70b3ca9462aad9f18ac14886
pmfoysal <pmfoysal@gmail.com>
2022-06-24 01:42:57 +0600

    UTC Timezone Added

--- utilities/getTodayTime.js
+++ utilities/getTodayTime.js
@@ -1,4 +1,6 @@
 module.exports = function getTodayTime() {
-   const date = new Date().toLocaleDateString('en-US');
+   const utcStr = new Date().toUTCString();
+   const utcArr = utcStr?.replaceAll(/[ ,]+/g, ',')?.split(',');
+   const date = `${utcArr[1]} ${utcArr[2]}, ${utcArr[3]}`;
    return new Date(date).getTime() / 1000;
 };
