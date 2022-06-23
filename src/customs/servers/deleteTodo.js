49de8dab116793257ff6d7d1ea3bcfdb9631b078
pmfoysal <pmfoysal@gmail.com>
2022-06-23 20:13:22 +0600

    deleteTodo Server Function Added

--- src/customs/servers/deleteTodo.js
+++ src/customs/servers/deleteTodo.js
@@ -1,14 +1,14 @@
 import { toast } from 'react-toastify';
 import pmaxios from '@middlewares/pmaxios';
 
-export default function editTodo(id, data, closer) {
-   const tId = toast.loading('Please wait! Your task is updating...');
+export default function deleteTodo(id, closer) {
+   const tId = toast.loading('Please wait! Your task is deleting...');
    pmaxios
-      .put(`/todo/${id}`, data)
+      .delete(`/todo${id}`)
       .then(res => {
          if (res?.data?.acknowledged) {
             toast.update(tId, {
-               render: 'Success! You task updated.',
+               render: 'Success! You task deleted.',
                type: 'success',
                isLoading: false,
                autoClose: 3000,
@@ -18,7 +18,7 @@ export default function editTodo(id, data, closer) {
       })
       .catch(error => {
          toast.update(tId, {
-            render: "Failed! You task isn't updated.",
+            render: "Failed! You task isn't deleted.",
             type: 'error',
             isLoading: false,
             autoClose: 3000,
