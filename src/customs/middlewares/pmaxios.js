6411822465fa97577d2482fddfeae7df23386995
pmfoysal <pmfoysal@gmail.com>
2022-06-23 13:33:59 +0600

    Axios Interceptors Added

---
+++ src/customs/middlewares/pmaxios.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const pmaxios = axios.create({
+   baseURL: process.env.BASE_API_URL,
+});
+
+pmaxios.interceptors.request.use(
+   function (config) {
+      if (!config?.headers?.authorization) {
+         config.headers.authorization = `Bearer ${window.localStorage.getItem('userToken')}`;
+      }
+      return config;
+   },
+   function (error) {
+      return Promise.reject(error);
+   }
+);
+
+pmaxios.interceptors.response.use(
+   function (response) {
+      return response;
+   },
+   function (error) {
+      return Promise.reject(error);
+   }
+);
+
+export default pmaxios;
