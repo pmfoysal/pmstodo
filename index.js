a95c7e42297a5a0a54950b20d847b650f9152e3b
pmfoysal <pmfoysal@gmail.com>
2022-06-23 12:00:29 +0600

    verifyUser Function Added

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ app.use(cors({ origin: '*' }));
 
 // [===>>>) Verification Middlewares Starts Here (<<<===] //
 
+function verifyUser(req, res, nex) {
+   const auth = req?.headers?.authorization;
+   const res1 = { status: 401, message: "You don't have Authorization to access this API!" };
+   const res2 = { status: 403, message: 'You have invalid Token to access this API!' };
+
+   if (!auth) return res.status(401).send(res1);
+   const token = auth.split(' ')[1];
+
+   jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+      if (error) return res.status(403).send(res2);
+      req.user = { ...decoded };
+      nex();
+   });
+}
+
 function verifyAdmin(req, res, nex) {
    if (req?.user?.email !== process.env.ADMIN_EMAIL) {
       return res.status(403).send({
