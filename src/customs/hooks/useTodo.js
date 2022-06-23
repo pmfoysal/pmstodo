e7c0f2023d3468f2fa34f7a7ab2a31c68a66b8f9
pmfoysal <pmfoysal@gmail.com>
2022-06-23 19:47:52 +0600

    useTodo Hook Added

---
+++ src/customs/hooks/useTodo.js
@@ -0,0 +1,9 @@
+import { useQuery } from 'react-query';
+import pmaxios from '@middlewares/pmaxios';
+
+export default function useTodo(id) {
+   return useQuery(['todo', id], async () => {
+      const { data } = await pmaxios.get(`/todo/${id}`);
+      return data;
+   });
+}
