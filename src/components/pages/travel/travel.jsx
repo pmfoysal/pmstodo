9afb3fdf64d48599485f98fe601ce9da117fbead
pmfoysal <pmfoysal@gmail.com>
2022-06-24 18:56:20 +0600

    Travel Page Component Updated

--- src/components/pages/travel/travel.jsx
+++ src/components/pages/travel/travel.jsx
@@ -1,9 +1,27 @@
+import Todo from '@partials/todo';
+import useTodos from '@hooks/useTodos';
 import React, { Fragment } from 'react';
+import EmptyText from '@shared/emptyText';
+import PageTitle from '@shared/pageTitle';
+import PageLoader from '@helpers/pageLoader';
+import { TodayContent } from '../today/today.styled';
 
 export default function Travel() {
+   const { isLoading, data: todos = [] } = useTodos('travel');
+
    return (
       <Fragment>
-         <div>Travel Page</div>
+         <PageTitle>travel todos</PageTitle>
+         {isLoading ? (
+            <PageLoader />
+         ) : (
+            <TodayContent>
+               {!todos?.length && <EmptyText />}
+               {todos?.map(todo => (
+                  <Todo key={todo?._id} data={todo} />
+               ))}
+            </TodayContent>
+         )}
       </Fragment>
    );
 }
