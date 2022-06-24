c490bc430d29525f2bfc577b10ec4ddaee2ceae1
pmfoysal <pmfoysal@gmail.com>
2022-06-24 18:55:28 +0600

    Personal Page Component Updated

--- src/components/pages/personal/personal.jsx
+++ src/components/pages/personal/personal.jsx
@@ -1,9 +1,27 @@
+import Todo from '@partials/todo';
+import useTodos from '@hooks/useTodos';
 import React, { Fragment } from 'react';
+import EmptyText from '@shared/emptyText';
+import PageTitle from '@shared/pageTitle';
+import PageLoader from '@helpers/pageLoader';
+import { TodayContent } from '../today/today.styled';
 
 export default function Personal() {
+   const { isLoading, data: todos = [] } = useTodos('personal');
+
    return (
       <Fragment>
-         <div>Personal Page</div>
+         <PageTitle>personal todos</PageTitle>
+         {isLoading ? (
+            <PageLoader />
+         ) : (
+            <TodayContent>
+               {!todos?.length && <EmptyText />}
+               {todos?.map(todo => (
+                  <Todo key={todo?._id} data={todo} />
+               ))}
+            </TodayContent>
+         )}
       </Fragment>
    );
 }
