64cd2bb4a58de601d5fa2f79744519b0f3659742
pmfoysal <pmfoysal@gmail.com>
2022-06-25 00:52:40 +0600

    getDateTime Issues Fixed

--- utilities/getDateTime.js
+++ utilities/getDateTime.js
@@ -1,7 +1,7 @@
 module.exports = function getDateTime(date) {
    if (!date) return 0;
    const iso = new Date(date).toISOString();
-   const [date] = iso?.split('T');
-   const local = `${date}T00:00:00.000Z`;
+   const [dddd] = iso?.split('T');
+   const local = `${dddd}T00:00:00.000Z`;
    return new Date(local).getTime();
 };
