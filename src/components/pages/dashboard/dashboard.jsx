ca0262c5bd359dbe262eed899adbcccd6b8cea00
pmfoysal <pmfoysal@gmail.com>
2022-06-25 12:44:05 +0600

    Dashboard Todos Card Added

--- src/components/pages/dashboard/dashboard.jsx
+++ src/components/pages/dashboard/dashboard.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import useStats from '@hooks/useStats';
 import getStats from '@servers/getStats';
 import PageTitle from '@shared/pageTitle';
 import InfoCard from './partials/infoCard';
-import { DashboardCards, DashboardContainer } from './dashboard.styled';
+import { DashboardCards, DashboardContainer, DashboardTodoArea, DashboardTodos, DashboardTodosContent } from './dashboard.styled';
+import Details from '../details/details';
+import useAllTodos from '@hooks/useAllTodos';
+import EmptyText from '@shared/emptyText';
+import Todo from '@partials/todo';
+import PageLoader from '@helpers/pageLoader';
 
 export default function Dashboard() {
-   const { isLoading, data: stats = {} } = useStats();
+   const { isLoading: statsLoading, data: stats = {} } = useStats();
+   const { isLoading: todosLoading, data: todos = [] } = useAllTodos();
 
    function renderStats() {
       const allStats = getStats(stats);
@@ -22,7 +28,25 @@ export default function Dashboard() {
    return (
       <DashboardContainer>
          <PageTitle>overall statistics</PageTitle>
-         <DashboardCards>{renderStats()};</DashboardCards>
+         <DashboardCards className={statsLoading}>{statsLoading ? <PageLoader /> : renderStats()}</DashboardCards>
+         <DashboardTodoArea>
+            <DashboardTodosContent>
+               <PageTitle>all users todos</PageTitle>
+               <DashboardTodos>
+                  {todosLoading ? (
+                     <PageLoader />
+                  ) : (
+                     <Fragment>
+                        {!todos?.length && <EmptyText />}
+                        {todos?.map(todo => (
+                           <Todo key={todo?._id} data={todo} />
+                        ))}
+                     </Fragment>
+                  )}
+               </DashboardTodos>
+            </DashboardTodosContent>
+            <Details />
+         </DashboardTodoArea>
       </DashboardContainer>
    );
 }
--- src/components/pages/dashboard/dashboard.styled.js
+++ src/components/pages/dashboard/dashboard.styled.js
@@ -11,13 +11,30 @@ export const DashboardCards = styled.div`
    display: grid;
    grid-template-columns: repeat(2, 1fr);
    gap: 2rem;
+
+   &.true {
+      grid-template-columns: 1fr;
+   }
 `;
 
 export const DashboardTodoArea = styled.div`
    display: grid;
    grid-template-columns: repeat(2, 1fr);
    gap: 2rem;
-   position: sticky;
-   top: 0;
-   left: 0;
+   margin-top: 4rem;
+   height: 100%;
+   overflow: hidden;
+`;
+
+export const DashboardTodosContent = styled.div`
+   max-height: 100%;
+   width: 100%;
+   overflow-x: hidden;
+   overflow-y: auto;
+`;
+
+export const DashboardTodos = styled.article`
+   display: flex;
+   flex-direction: column;
+   gap: 1rem;
 `;
